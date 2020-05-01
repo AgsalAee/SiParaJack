1bda72bb019bf87d7da010fdafeab06cb48fb591
AgsalAee <agsal.agsal9@gmail.com>
2020-05-01 23:00:52 +0700

    admin and user
    combine user and admin

---
+++ siparajack/README.md
@@ -0,0 +1,18 @@
+### Web Konveksi
+
+Selamat atas lulus nya Muhammad Nur Huda S.Kom \ :v /
+
+> Untuk penggunaan aplikasi tanya pemiliknya langsung.
+
+```
+1. Clone Repository ini
+```
+
+```
+2. Simpan di ./xampp/htdocs/NAMA_PROJECT
+```
+
+
+```
+3. Atur kredensial database di ./applications/config/databse.php
+```
---
+++ siparajack/application/views/Pemesanan.php
@@ -0,0 +1,31 @@
+<body>
+    <!-- mian-content -->
+    <section class="mian-content">
+
+        <!-- /header-top-->
+        <div class="header-top">
+            <!-- /banner-info-->
+            <div class="banner-content text-center text-white">
+                <h3 class="text-uppercase"> Konveksi</h3>
+                <h3>Paramount Jacket</h3>
+
+                <div class="banner-high-lights text-center">
+
+                    <div class="ban-text text-center py-3">
+
+                    </div>
+                </div>
+            </div>
+            <!-- //banner-info-->
+        </div>
+        <!-- //header-top-->
+    </section>
+    <section class="banner-bottom py-lg-5 py-md-5 py-3" id="feature">
+        <div class="container">
+            <div class="inner-sec py-lg-5 py-3">
+                <h2 class="tittle text-center"> Services</h2>
+                <div class="row mt-lg-5 mt-md-4 mt-4">
+                </div>
+            </div>
+        </div>
+    </section>
